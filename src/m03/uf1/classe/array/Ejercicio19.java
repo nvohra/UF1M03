3c69d6339011a6d8b612d6d962ee3e58f5536b33
USER <USER@172.16.0.223>
2014-01-14 19:59:18 +0100

    first commit

--- src/m03/uf1/classe/array/Ejercicio19.java
+++ src/m03/uf1/classe/array/Ejercicio19.java
@@ -17,17 +17,27 @@ public class Ejercicio19 {
         Scanner s = new Scanner(System.in);
         System.out.println("Introduce el numero de alumnos:");
         int n = s.nextInt();
+        //aunq float guarda solo las notas, la columna 3 se añade para guardar las medias
         float[][] notas = new float[n][3];
         String[] nombre = new String[n];
+        System.out.println("Introduce los porcentajes del primer parcial y el del segundo:");
+        float por1 = s.nextFloat();
+        float por2 = s.nextFloat();
         System.out.println("Introduce los nombres de los " + n + " alumnos mas sus respectivas notas de los 2 parciales:");
         for (int i = 0; i < n; i++) {
-            float media = 0;
+            float mediatotal = 0;
             nombre[i] = s.next();
             for (int j = 0; j < 2; j++) {
                 notas[i][j] = s.nextInt();
-                media += notas[i][j];
+                if (j == 0) {
+                    mediatotal = (notas[i][j]*por1)/100;
+                } else {
+                 mediatotal += ((notas[i][j]*por2)/100);   
+                }
             }
-            notas[i][2] = media/2;
+//            notas[i][2] = mediatotal/2;
+            notas[i][2] = mediatotal;
+            System.out.println(mediatotal);
         }
         //sacar los alumnos suspensos y aprobados
         String sus = "";
